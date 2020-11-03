862d8c444876ab83e4a0e28f5a32e2a70393f4a0
Mateus Soares <fvs.mateus@gmail.com>
2020-11-03 08:48:02 -0300

    m - modificações na classe ambiente.

--- ambiente.py
+++ ambiente.py
@@ -7,26 +7,36 @@ class Ambiente:
         :param dic_func: dicionario com infromacoes das funcoes
 
         struct da pilha:
-        [nome_da_funcao, classe_da_funcao, se_e_static, variaveis]
+        [nome_da_funcao, dict_variaveis, var_de_retorno]
         '''
         #pilha de ambiente
         self.ambiente = list()
 
         #definicao dos ambientes
-        self.ambiente_local = list()
-        self.ambiente_base = list()
-        self.ambiente_instancia = list()
-        self.ambiente_classe = list()
+        self.ambiente_base = dict()
+        self.ambiente_instancia = dict()
+        self.ambiente_classe = dict()
 
         self.dic_classe = dic_classe
         self.dic_func = dic_func
 
-        pass
+        self._inicia_ambientes()
 
     def _inicia_ambientes(self):
 
-        for func in self.dic_func:
-            print(func)
+        print(self.dic_func)
+        for func_name in self.dic_func:
+
+            values = self.dic_func[func_name]
+
+            #se for funcao sem classe
+            if values[0] is None:
+
+                dados = {func_name: [None, None, values[1], values[2]]}
+
+                print(func_name)
+
+
 
         pass
 
